3531785a8fd43712091e31719e1e106b3b40d509
sumAlbert <982675088@qq.com>
2017-06-16 23:33:10 +0800

    some changes by ZQ

--- app/src/main/java/com/example/dell2/androidexamgroup/Test6_ZQ.java
+++ app/src/main/java/com/example/dell2/androidexamgroup/Test6_ZQ.java
@@ -1,8 +1,11 @@
-package collector;
+package com.example.dell2.androidexamgroup;
+
+import collector.BaseActivity;
 
 /**
  * Created by dell2 on 2017/6/16.
  */
 
 public class Test6_ZQ extends BaseActivity {
+
 }
