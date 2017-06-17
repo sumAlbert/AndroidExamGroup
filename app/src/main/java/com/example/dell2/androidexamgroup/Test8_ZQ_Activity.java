3c3b50704b47bcc863c9084d6e7bfa3cc95019e6
sumAlbert <982675088@qq.com>
2017-06-17 11:01:09 +0800

    some changes by ZQ

--- app/src/main/java/com/example/dell2/androidexamgroup/Test8_ZQ_Activity.java
+++ app/src/main/java/com/example/dell2/androidexamgroup/Test8_ZQ_Activity.java
@@ -1,13 +1,22 @@
 package com.example.dell2.androidexamgroup;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adapt.Test8_ZQ_Adapt;
 import collector.BaseActivity;
 import db.DbHelp_ZQ;
 
@@ -17,6 +26,10 @@ import db.DbHelp_ZQ;
 
 public class Test8_ZQ_Activity extends BaseActivity {
     private DbHelp_ZQ dbHelp_zq;
+    private ListView test8_ZQ_ListView;
+    private Test8_ZQ_Adapt test8_zq_adapt;
+    private List<String> data=new ArrayList<>();
+    private int pos=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +42,37 @@ public class Test8_ZQ_Activity extends BaseActivity {
     public void init(){
         dbHelp_zq=new DbHelp_ZQ(this,"Name.db",null,1);
         displayDb();
+        test8_ZQ_ListView=(ListView)findViewById(R.id.test8_ZQ_ListView);
+        test8_zq_adapt=new Test8_ZQ_Adapt(Test8_ZQ_Activity.this,R.layout.listview_test8_zq,data);
+        test8_ZQ_ListView.setAdapter(test8_zq_adapt);
+        test8_ZQ_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(Test8_ZQ_Activity.this);
+                pos=i;
+                dialog.setTitle("Delete this item?");
+                dialog.setMessage(data.get(i));
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteItem(data.get(pos));
+                        data.clear();
+                        displayDb();
+                        test8_zq_adapt=new Test8_ZQ_Adapt(Test8_ZQ_Activity.this,R.layout.listview_test8_zq,data);
+                        test8_ZQ_ListView.setAdapter(test8_zq_adapt);
+                    }
+                });
+                dialog.setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(Test8_ZQ_Activity.this,"delete cancel",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
     }
     public static void actionStart(Context context){
         Intent intent=new Intent(context,Test8_ZQ_Activity.class);
@@ -42,10 +86,15 @@ public class Test8_ZQ_Activity extends BaseActivity {
             do{
                 i++;
                 String name=cursor.getString(cursor.getColumnIndex("name"));
-                Log.d("Test8_ZQ","Name"+name);
+                data.add(name);
             }while(cursor.moveToNext());
         }
         cursor.close();
         Toast.makeText(Test8_ZQ_Activity.this,String.valueOf(i),Toast.LENGTH_SHORT).show();
+    }
+    public void deleteItem(String str){
+        SQLiteDatabase sqLiteDatabase=dbHelp_zq.getWritableDatabase();
+        sqLiteDatabase.delete("Name"," name = ?",new String[] {str});
+        Toast.makeText(Test8_ZQ_Activity.this,"delete success",Toast.LENGTH_SHORT).show();
     }
 }
