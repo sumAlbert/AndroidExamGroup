fff5db51b4d870d6a0a160ccebda78a3b5d8974c
sumAlbert <982675088@qq.com>
2017-06-18 10:38:55 +0800

    some changes by ZQ

--- app/src/main/java/com/example/dell2/androidexamgroup/MainActivity.java
+++ app/src/main/java/com/example/dell2/androidexamgroup/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     private LinearLayout test6;
     private LinearLayout test7;
     private LinearLayout test8;
+    private LinearLayout test3_ZQ;
 
 
     @Override
@@ -37,6 +38,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         test6=(LinearLayout)findViewById(R.id.test6);
         test7=(LinearLayout)findViewById(R.id.test7);
         test8=(LinearLayout)findViewById(R.id.test8);
+        test3_ZQ=(LinearLayout)findViewById(R.id.test3_ZQ);
         test1.setOnClickListener(this);
         test2.setOnClickListener(this);
         test3.setOnClickListener(this);
@@ -45,6 +47,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         test6.setOnClickListener(this);
         test7.setOnClickListener(this);
         test8.setOnClickListener(this);
+        test3_ZQ.setOnClickListener(this);
     }
 
     public void onClick(View view){
@@ -72,6 +75,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                 break;
             case R.id.test8:
                 Test8_ZQ_Activity.actionStart(MainActivity.this);
+                break;
+            case R.id.test3_ZQ:
+                Test3_ZQ_Activity.actionStart(MainActivity.this);
                 break;
             default:
                 break;
